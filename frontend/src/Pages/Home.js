6585931ccd9484dcff4545a78b8578638a5e1d79
julia caruso <juliacaruso779@gmail.com>
2024-06-06 09:41:51 -0300

    julia navbar

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,13 +1,46 @@
 // Home
 import React from "react";
-
+import "../css/home.css";
+import "../css/navegacao.css";
+import { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+// import ExampleCarouselImage from 'components/ExampleCarouselImage';
 
 function Home(){
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
   return (
     <>
-      <div>
-        <h2>Página Home</h2>
-      </div>
+ 
+     <Carousel activeIndex={index} onSelect={handleSelect} className="carosel">
+      <Carousel.Item>
+        {/* <ExampleCarouselImage text="First slide" /> */}
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        {/* <ExampleCarouselImage text="Second slide" /> */}
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        {/* <ExampleCarouselImage text="Third slide" /> */}
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
     </>
   );
 };
